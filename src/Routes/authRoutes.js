f849f19f6f9a780a578c806050f266f7d0b24a25
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-12-11 20:57:06 +0600

    updated backend

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,15 +1,12 @@
 import express from "express";
 import { getMe, handleFirebaseAuth, logout, updateUser } from "../Controllers/authController.js";
-import { verifyJWT } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Public Routes
 router.post("/firebase-login", handleFirebaseAuth);
 router.post("/logout", logout);
-
-// Protected Routes
-router.get("/me", verifyJWT, getMe);
-router.put("/update", verifyJWT, updateUser);
+router.get("/me", getMe);
+router.put("/update", updateUser);
 
 export default router;
+
