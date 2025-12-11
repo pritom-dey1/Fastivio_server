b6868a3b788c7d70367611da020da8410c6ebf62
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-12-11 19:50:50 +0600

    added frontend domain

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://admirable-sfogliatella-88913a.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
